d080fb00c0467c6d99be5f7892fdbdcd74cbd4c7
Aunsetre <aunsetre@outlook.com>
2018-06-07 19:33:28 +0800

    添加用户管理

--- src/main/resources/static/layui/index.js
+++ src/main/resources/static/layui/index.js
@@ -3,25 +3,20 @@ layui.use('element', function(){
     var element = layui.element;
 
 });
-layui.use('table', function(){
-    var table = layui.table;
+layui.use(['layer', 'form'], function(){
+    var layer = layui.layer
+        ,form = layui.form;
 
-    //第一个实例
-    table.render({
-        elem: '#demo'
-        ,height: 315,
-        width: 800
-        ,url: '/GetUserAll' //数据接口
-        ,page: true //开启分页
-        ,cols: [[ //表头
-            {field: 'id', title: 'ID', width:80, sort: true, fixed: 'left'}
-            ,{field: 'name', title: '用户名', width:80, sort:true}
-            ,{field: 'pwd', title: '密码', width:80}
-            ,{field: 'email', title: '邮箱', width:80}
-            ,{field: 'Retime', title: '注册时间', width: 177}
-            ,{field: 'rid', title: '用户状态', width: 100, sort: true}
-            ,{field: 'logId', title: '授权', width: 80, sort: true}
-        ]]
+form.on('submit(add)', function(){
+    layer.open({
+        type: 2,
+        title: '新增信息',
+        shadeClose: false,
+        shade: 0.5,
+        area: ['40%','40%'],
+        content: localhostPath+"/common/Detail.html"
     });
+
+});
 
 });
